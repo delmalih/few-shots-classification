238cbb0c581f0a53e1025fd3ed1b5590ff089369
El Malih David <da.elmalih@gmail.com>
2020-11-12 00:24:14 +0100

    feat: implement constants

---
+++ constants.py
@@ -0,0 +1,56 @@
+##########################
+# Imports
+##########################
+
+
+import cv2
+
+
+##########################
+# Constants
+##########################
+
+
+# GENERAL
+VERBOSE = True
+BACKGROUND_LABEL = "Background"
+DEFAULT_FEATURE_EXTRACTOR = cv2.xfeatures2d.SURF_create(extended=True)
+
+
+# MATCHER
+DEFAULT_MATCHER_INDEX_PARAMS = {
+    'M': 15,
+    'indexThreadQty': 8,
+    'efConstruction': 100,
+    'post': 0}
+DEFAULT_MATCHER_QUERY_PARAMS = {
+    'efSearch': 100}
+
+
+# CLASSIFIER (GENERAL)
+DEFAULT_CLASSIFIER_IMAGE_SIZE = 256
+DEFAULT_CLASSIFIER_KEYPOINT_STRIDE = 8
+DEFAULT_CLASSIFIER_KEYPOINT_SIZES = [12, 24, 32, 48, 56, 64]
+
+
+# CLASSIFIER (CUSTOM)
+DEFAULT_CLASSIFIER_CUSTOM_MATCHER_PATH = "./files/matcher-classifier-custom.hnsw"
+DEFAULT_CLASSIFIER_CUSTOM_K_NN = 1
+DEFAULT_CLASSIFIER_CUSTOM_SCORE_SIGMA = 0.2
+DEFAULT_CLASSIFIER_CUSTOM_FORCE_MATCHER_COMPUTE = False
+
+
+# CLASSIFIER (BOW)
+DEFAULT_CLASSIFIER_BOW_IMAGE_SIZE = 256
+DEFAULT_CLASSIFIER_BOW_VOCAB_SIZE = 2000
+DEFAULT_CLASSIFIER_BOW_VOCAB_PATH = "./files/vocab.pkl"
+DEFAULT_CLASSIFIER_BOW_CATALOG_FEATURES_PATH = "./files/catalog-features.pkl"
+DEFAULT_CLASSIFIER_BOW_FORCE_VOCAB_COMPUTE = False
+DEFAULT_CLASSIFIER_BOW_FORCE_CATALOG_FEATURES_COMPUTE = False
+
+
+# OTHER
+# CATALOG_IMAGES_PATH = "../Images/Catalog"
+# CLASSIFICATION_QUERY_IMAGES_PATH = "../Images/Query"
+# CLASSIFICATION_GROUND_TRUTH_PATH = "../Images/ground_truth_classification.json"
+# DETECTOR_QUERY_IMAGES_PATH = "../Images/Query Raw/JPEGImages"
