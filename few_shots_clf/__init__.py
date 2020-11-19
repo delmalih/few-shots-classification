8137bbaf8ef221505cc366f59a9ed2e14af5fa74
El Malih David <da.elmalih@gmail.com>
2020-11-19 16:06:10 +0100

    feat: implement fm_classifier

---
+++ few_shots_clf/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=pointless-string-statement
+
+from .fm_classifier import FMClassifier
+from .bow_classifier import BOWClassifier
+
+""" Top-level package for Few Shots Classification Library. """
+
+__author__ = "David El Malih"
+__version__ = "0.0.0"
