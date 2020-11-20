ab8b286e25246b363aca78159817f88a054430fa
El Malih David <da.elmalih@gmail.com>
2020-11-21 00:55:33 +0100

    feat: update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 
 # Global
-from setuptools import setup
+import setuptools
 
 # Local
 import few_shots_clf
@@ -15,6 +15,7 @@ import few_shots_clf
 ##########################
 
 
+# Install requirements
 requirements = [
     "tqdm",
     "autopep8",
@@ -29,15 +30,41 @@ requirements = [
 ]
 
 
-setup(
+# Long description
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+# Download url
+GITHUB_URL = "https://github.com/delmalih/few-shots-classification"
+DOWNLOAD_VERSION = few_shots_clf.__version__.replace(".", "")
+DOWNLOAD_URL = f"{GITHUB_URL}/archive/v_{DOWNLOAD_VERSION}.tar.gz"
+
+
+setuptools.setup(
     name="few_shots_clf",
     version=few_shots_clf.__version__,
     author=few_shots_clf.__author__,
     author_email=few_shots_clf.__author_email__,
     description=few_shots_clf.__description__,
-    packages=["few_shots_clf", "few_shots_clf.test"],
-    url="http://pypi.python.org/pypi/few_shots_clf/",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=GITHUB_URL,
+    download_url=DOWNLOAD_URL,
+    packages=setuptools.find_packages(),
     license="LICENSE.txt",
-    long_description=open("README.md").read(),
     install_requires=requirements,
+    python_requires="==3.6",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords=[
+        "FEW-SHOTS",
+        "CLASSIFICATION",
+        "IMAGE",
+        "COMPUTER VISION"
+    ],
 )
