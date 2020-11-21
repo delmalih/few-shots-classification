2b3e3a2b9d4abdf969cb51822c60a275cfabe52d
El Malih David <da.elmalih@gmail.com>
2020-11-21 13:37:20 +0100

    feat: implement test_folder_with_one_image

--- few_shots_clf/__init__.py
+++ few_shots_clf/__init__.py
@@ -7,6 +7,6 @@ from .bow_classifier import BOWClassifier
 
 __author__ = "David El Malih"
 __author_email__ = "da.elmalih@gmail.com"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __description__ = "Few Shots Classification Library. " + \
                   "Classification task from a small amount of data."
--- few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
+++ few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
@@ -8,6 +8,10 @@ import os
 import shutil
 
 # Global
+import cv2
+import numpy as np
+
+# Local
 from few_shots_clf.utils import get_all_images_from_folder
 
 
@@ -23,7 +27,7 @@ def test_empty_folder():
     """[summary]
     """
     # Directory path
-    dir_path = f"{TEST_DIRECTORY_PATH}/test_empty_folder/"
+    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_empty_folder")
 
     # Create empty dir
     os.makedirs(dir_path)
@@ -43,13 +47,14 @@ def test_folder_with_no_images():
     """[summary]
     """
     # Directory path
-    dir_path = f"{TEST_DIRECTORY_PATH}/test_folder_with_no_images/"
+    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_folder_with_no_images")
 
     # Create empty dir
     os.makedirs(dir_path)
 
     # Add a file
-    with open(f"{dir_path}/tmp.txt", "w") as tmp_file:
+    file_path = os.path.join(dir_path, "tmp.txt")
+    with open(file_path, "w") as tmp_file:
         tmp_file.write("test")
 
     # Get all images from folder
@@ -57,6 +62,34 @@ def test_folder_with_no_images():
 
     # Assert
     assert len(paths) == 0
+
+    # Delete directory
+    shutil.rmtree(dir_path)
+    shutil.rmtree(TEST_DIRECTORY_PATH)
+
+
+def test_folder_with_one_image():
+    """[summary]
+    """
+    # Directory path
+    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_folder_with_one_image")
+
+    # Create empty dir
+    os.makedirs(dir_path)
+
+    # Create image
+    img = np.zeros((10, 10, 3))
+
+    # Add an image
+    img_path = os.path.join(dir_path, "tmp.jpg")
+    cv2.imwrite(img_path, img)
+
+    # Get all images from folder
+    paths = get_all_images_from_folder(dir_path)
+
+    # Assert
+    assert len(paths) == 1
+    assert img_path in paths
 
     # Delete directory
     shutil.rmtree(dir_path)
