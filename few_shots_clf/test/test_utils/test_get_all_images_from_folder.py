e626ac7555697c427241c253a680b85f5de64824
El Malih David <da.elmalih@gmail.com>
2020-11-21 13:32:10 +0100

    feat: implement test_folder_with_no_images

--- few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
+++ few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
@@ -16,11 +16,14 @@ from few_shots_clf.utils import get_all_images_from_folder
 ##########################
 
 
+TEST_DIRECTORY_PATH = "/tmp/test_few_shots_clf"
+
+
 def test_empty_folder():
     """[summary]
     """
     # Directory path
-    dir_path = "/tmp/test_few_shots_clf/test_empty_folder/"
+    dir_path = f"{TEST_DIRECTORY_PATH}/test_empty_folder/"
 
     # Create empty dir
     os.makedirs(dir_path)
@@ -33,3 +36,28 @@ def test_empty_folder():
 
     # Delete directory
     shutil.rmtree(dir_path)
+    shutil.rmtree(TEST_DIRECTORY_PATH)
+
+
+def test_folder_with_no_images():
+    """[summary]
+    """
+    # Directory path
+    dir_path = f"{TEST_DIRECTORY_PATH}/test_folder_with_no_images/"
+
+    # Create empty dir
+    os.makedirs(dir_path)
+
+    # Add a file
+    with open(f"{dir_path}/tmp.txt", "w") as tmp_file:
+        tmp_file.write("test")
+
+    # Get all images from folder
+    paths = get_all_images_from_folder(dir_path)
+
+    # Assert
+    assert len(paths) == 0
+
+    # Delete directory
+    shutil.rmtree(dir_path)
+    shutil.rmtree(TEST_DIRECTORY_PATH)
