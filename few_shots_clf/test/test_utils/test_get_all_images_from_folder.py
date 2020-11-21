f6b0061fa729f498577ce89f6ad5cfa1c93cd80e
El Malih David <da.elmalih@gmail.com>
2020-11-21 16:17:34 +0100

    feat: implement test_folder_with_multiple_images

--- few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
+++ few_shots_clf/test/test_utils/test_get_all_images_from_folder.py
@@ -13,6 +13,7 @@ import numpy as np
 
 # Local
 from few_shots_clf.utils import get_all_images_from_folder
+from few_shots_clf.test.test_utils import TEST_DIRECTORY_PATH
 
 
 ##########################
@@ -20,110 +21,101 @@ from few_shots_clf.utils import get_all_images_from_folder
 ##########################
 
 
-TEST_DIRECTORY_PATH = "/tmp/test_few_shots_clf"
-
-
 def test_empty_folder():
     """[summary]
     """
-    # Directory path
-    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_empty_folder")
-
-    # Create empty dir
-    os.makedirs(dir_path)
+    # Create dir
+    if not os.path.exists(TEST_DIRECTORY_PATH):
+        os.makedirs(TEST_DIRECTORY_PATH)
 
     # Get all images from folder
-    paths = get_all_images_from_folder(dir_path)
+    paths = get_all_images_from_folder(TEST_DIRECTORY_PATH)
 
     # Assert
     assert len(paths) == 0
-
-    # Delete directory
-    shutil.rmtree(dir_path)
-    shutil.rmtree(TEST_DIRECTORY_PATH)
 
 
 def test_folder_with_no_images():
     """[summary]
     """
-    # Directory path
-    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_folder_with_no_images")
-
-    # Create empty dir
-    os.makedirs(dir_path)
+    # Create dir
+    if not os.path.exists(TEST_DIRECTORY_PATH):
+        os.makedirs(TEST_DIRECTORY_PATH)
 
     # Add a file
-    file_path = os.path.join(dir_path, "tmp.txt")
+    file_path = os.path.join(TEST_DIRECTORY_PATH, "tmp.txt")
     with open(file_path, "w") as tmp_file:
         tmp_file.write("test")
 
     # Get all images from folder
-    paths = get_all_images_from_folder(dir_path)
+    paths = get_all_images_from_folder(TEST_DIRECTORY_PATH)
 
     # Assert
     assert len(paths) == 0
 
-    # Delete directory
-    shutil.rmtree(dir_path)
+    # Delete
+    os.remove(file_path)
     shutil.rmtree(TEST_DIRECTORY_PATH)
 
 
 def test_folder_with_one_image():
     """[summary]
     """
-    # Directory path
-    dir_path = os.path.join(TEST_DIRECTORY_PATH, "test_folder_with_one_image")
-
-    # Create empty dir
-    os.makedirs(dir_path)
+    # Create dir
+    if not os.path.exists(TEST_DIRECTORY_PATH):
+        os.makedirs(TEST_DIRECTORY_PATH)
 
     # Create image
     img = np.zeros((10, 10, 3))
 
     # Add an image
-    img_path = os.path.join(dir_path, "tmp.jpg")
+    img_path = os.path.join(TEST_DIRECTORY_PATH, "tmp.jpg")
     cv2.imwrite(img_path, img)
 
     # Get all images from folder
-    paths = get_all_images_from_folder(dir_path)
+    paths = get_all_images_from_folder(TEST_DIRECTORY_PATH)
 
     # Assert
     assert len(paths) == 1
     assert img_path in paths
 
-    # Delete directory
-    shutil.rmtree(dir_path)
+    # Delete
+    os.remove(img_path)
     shutil.rmtree(TEST_DIRECTORY_PATH)
 
 
 def test_folder_with_multiple_images():
     """[summary]
     """
-    # Directory path
-    dir_path = os.path.join(TEST_DIRECTORY_PATH,
-                            "test_folder_with_multiple_images")
-
-    # Create empty dir
-    os.makedirs(dir_path)
+    # Create dir
+    if not os.path.exists(TEST_DIRECTORY_PATH):
+        os.makedirs(TEST_DIRECTORY_PATH)
 
     # Number of images
     nb_images = 10
 
     # Loop image creation
+    img_paths = []
     for k in range(nb_images):
         # Create image
         img = np.zeros((10, 10, 3))
 
         # Add image
-        img_path = os.path.join(dir_path, f"tmp{k}.jpg")
+        img_path = os.path.join(TEST_DIRECTORY_PATH, f"tmp{k}.jpg")
         cv2.imwrite(img_path, img)
 
+        # Update img_paths list
+        img_paths.append(img_path)
+
     # Get all images from folder
-    paths = get_all_images_from_folder(dir_path)
+    paths = get_all_images_from_folder(TEST_DIRECTORY_PATH)
 
     # Assert
-    assert len(paths) == 10
+    assert len(paths) == nb_images
+    for img_path in img_paths:
+        assert img_path in paths
 
-    # Delete directory
-    shutil.rmtree(dir_path)
+    # Delete
+    for img_path in img_paths:
+        os.remove(img_path)
     shutil.rmtree(TEST_DIRECTORY_PATH)
