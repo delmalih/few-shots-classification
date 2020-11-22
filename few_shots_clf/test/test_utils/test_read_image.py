4f073576d3ca05198eb672fe182e1ae0ba674449
El Malih David <da.elmalih@gmail.com>
2020-11-22 20:51:36 +0100

    feat: implement test_fixed_width

--- few_shots_clf/test/test_utils/test_read_image.py
+++ few_shots_clf/test/test_utils/test_read_image.py
@@ -14,6 +14,7 @@ import numpy as np
 # Local
 from few_shots_clf.utils import read_image
 from few_shots_clf.test.test_utils import TEST_DIRECTORY_PATH
+from few_shots_clf.test.test_utils import empty_dir
 
 
 ##########################
@@ -24,9 +25,8 @@ from few_shots_clf.test.test_utils import TEST_DIRECTORY_PATH
 def test_fixed_size():
     """[summary]
     """
-    # Create dir
-    if not os.path.exists(TEST_DIRECTORY_PATH):
-        os.makedirs(TEST_DIRECTORY_PATH)
+    # Empty dir
+    empty_dir()
 
     # Create image
     img_h = np.random.randint(10, 100)
@@ -44,6 +44,34 @@ def test_fixed_size():
     # Assert
     assert resized_w == 50
     assert resized_h == 50
+
+    # Delete
+    os.remove(img_path)
+    shutil.rmtree(TEST_DIRECTORY_PATH)
+
+
+def test_fixed_width():
+    """[summary]
+    """
+    # Empty dir
+    empty_dir()
+
+    # Create image
+    img_h = np.random.randint(10, 100)
+    img_w = np.random.randint(10, 100)
+    img = np.zeros((img_h, img_w, 3))
+
+    # Save image
+    img_path = os.path.join(TEST_DIRECTORY_PATH, "tmp.jpg")
+    cv2.imwrite(img_path, img)
+
+    # Resize img
+    resized_img = read_image(img_path, width=50)
+    resized_h, resized_w, _ = resized_img.shape
+
+    # Assert
+    assert resized_w == 50
+    assert resized_h == int(50 * img_h / img_w)
 
     # Delete
     os.remove(img_path)
