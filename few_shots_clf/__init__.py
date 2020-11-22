7663ef9af785e12d3228114704795dd4a5afd899
El Malih David <da.elmalih@gmail.com>
2020-11-23 00:59:46 +0100

    feat: remove bow_classifier

--- few_shots_clf/__init__.py
+++ few_shots_clf/__init__.py
@@ -1,12 +1,11 @@
 # pylint: disable=pointless-string-statement
 
 from .fm_classifier import FMClassifier
-from .bow_classifier import BOWClassifier
 
 """ Top-level package for Few Shots Classification Library. """
 
 __author__ = "David El Malih"
 __author_email__ = "da.elmalih@gmail.com"
-__version__ = "1.0.5"
+__version__ = "1.0.7"
 __description__ = "Few Shots Classification Library. " + \
                   "Classification task from a small amount of data."
