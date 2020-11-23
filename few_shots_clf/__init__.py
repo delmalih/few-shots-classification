19d8761eb3924e2ff1afcd077dfc55be7ab17dc0
El Malih David <da.elmalih@gmail.com>
2020-11-23 10:51:29 +0100

    feat: update version

--- few_shots_clf/__init__.py
+++ few_shots_clf/__init__.py
@@ -6,6 +6,6 @@ from .fm_classifier import FMClassifier
 
 __author__ = "David El Malih"
 __author_email__ = "da.elmalih@gmail.com"
-__version__ = "1.0.6"
+__version__ = "1.0.8"
 __description__ = "Few Shots Classification Library. " + \
                   "Classification task from a small amount of data."
