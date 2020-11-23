8d43d1d2bafa6649fc4b191854ab7ba9259a6a62
El Malih David <da.elmalih@gmail.com>
2020-11-23 01:13:32 +0100

    feat: update setup

--- setup.py
+++ setup.py
@@ -44,12 +44,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="LICENSE.txt",
     install_requires=requirements,
-    python_requires=">=3.6, <=3.7",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
     ],
     keywords=[
         "FEW-SHOTS",
