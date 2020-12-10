f18077c59022df1c572b53c75fde9de9c5eabf3b
El Malih David <da.elmalih@gmail.com>
2020-12-10 15:49:36 +0100

    docs: add documentation for get_iterator

--- few_shots_clf/utils/get_iterator.py
+++ few_shots_clf/utils/get_iterator.py
@@ -1,8 +1,10 @@
-
 ##########################
 # Imports
 ##########################
 
+
+# Built-in
+from typing import Iterable
 
 # Global
 from tqdm import tqdm
@@ -13,16 +15,18 @@ from tqdm import tqdm
 ##########################
 
 
-def get_iterator(iterator, verbose=True, description=""):
-    """[summary]
+def get_iterator(iterator: Iterable, verbose=True, description="") -> Iterable:
+    """Turns a given iterator into a tqdm iterator when verbose is True.
 
     Args:
-        iterator ([type]): [description]
-        verbose (bool, optional): [description]. Defaults to True.
-        description (str, optional): [description]. Defaults to "".
+        iterator (Iterable): The given iterator.
+        verbose (bool, optional): True to turn the iterator into a tqdm one.
+                                  Defaults to True.
+        description (str, optional): The description of the tqdm iterator.
+                                     Defaults to "".
 
     Returns:
-        [type]: [description]
+        Iterable: The iterator tqdmed (or not depending on the verbose variable).
     """
     if verbose:
         iterator = tqdm(iterator, desc=description)
